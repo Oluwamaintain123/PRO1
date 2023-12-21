6c9e34bfd25b93c53142d05572923744f6acee7d
Oluwamaintain123 <137342690+Oluwamaintain123@users.noreply.github.com>
2023-12-21 02:09:23 -1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const bars = document.querySelector('.barss');
+const navMenu = document.querySelector('.nav-menu');
+const change = document.querySelector('.fa-bars');
+
+bars.addEventListener("click", ()  => {
+    bars.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
